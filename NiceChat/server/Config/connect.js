884549100272d5a393bc0f72c65b323354301df4
NiceAmi <amimakeup@gmail.com>
2024-08-09 23:39:34 +0300

    Update connect.js

--- NiceChat/server/Config/connect.js
+++ NiceChat/server/Config/connect.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectToDb = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/Chat").then(()=>{
+const connectToDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to the database");
-    })
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectToDb;
