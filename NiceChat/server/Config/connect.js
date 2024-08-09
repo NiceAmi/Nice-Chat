770fa4582290b88571d496ed7537da6386119f49
NiceAmi <amimakeup@gmail.com>
2024-08-10 00:09:26 +0300

    Update connect.js

--- NiceChat/server/Config/connect.js
+++ NiceChat/server/Config/connect.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to the database");
     } catch (error) {
         console.error("Failed to connect to the database:", error);
